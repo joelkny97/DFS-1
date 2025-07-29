27b429adc749a1489955c79fe62fbeeb301cdc71
Joel <joelkny97@gmail.com>
2025-07-29 17:50:06 -0400

    Completed DFS-1

---
+++ Exercise_1.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<pair<int, int>> dirs;
+    int m;
+    int n;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}}; // U D L R
+
+        m = image.size();
+        n = image[0].size();
+
+        int oldColor = image[sr][sc];
+
+        // if the color is the same as the old color, we return the image as it is
+        if (color == oldColor) return image;
+        
+        // start the DFS from the given cell
+        dfs(image, sr, sc, color, oldColor);
+
+        return image;
+        
+    }
+private:
+    void dfs(vector<vector<int>>& image, int row, int col, const int &color, int oldColor){
+        // base case if the current cell is out of bounds or if the color is not the same as the old color
+        if ( row < 0 || col< 0 || row >=m || col >= n  || image[row][col] != oldColor ){
+            return;
+        }
+
+        // change color of the current cell to the new color
+        image[row][col] = color;
+        
+        // exlpore all four directions 
+        for(const auto& d: dirs){
+            
+            int nr = row + d.first;
+            int nc = col + d.second; 
+
+
+            // if the new cell is within bounds and has the old color we continue the DFS
+            // This ensures we only change the color of connected components and does not start DFS on cells that are already colored
+            if (nr >= 0 && nc >= 0 && nr < m && nc < n ){
+                if ( image[nr][nc] == oldColor) {
+
+                    dfs(image, nr, nc, color, oldColor);
+                }
+            }
+
+        }
+
+
+    }
+};
---
+++ Exercise_2.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<pair<int, int>> dirs;
+    int m;
+    int n;
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        if (mat.size() == 0 ){
+            return mat;
+        }
+
+        dirs = {{-1,0}, {1,0}, {0,-1}, {0,1} }; // U D L R
+
+        m = mat.size();
+        n = mat[0].size();
+
+        std::queue<pair<int,int>> q;
+
+        // initialize the queue with all the cells that have value 0
+        // and mark all cells with value 1 as -1 to indicate they are unvisited
+        // This will help us to only process cells that are initially 0 and to track the distance from the nearest 0
+        // This is a BFS approach to find the shortest distance from each cell to the nearest 0
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n ; j++){
+                if (mat[i][j] == 0){
+                    q.push({i,j});
+                }
+                else{
+                    mat[i][j] *= -1;
+                }
+            }
+        }
+
+        // BFS to update the distance of each cell to the nearest 0
+        while (!q.empty()) {
+            pair<int,int> curr = q.front();
+            q.pop();
+            
+            // explore all four directions from the current cell
+            
+            
+            for (const auto& d: dirs){
+                int nr = curr.first +d.first;
+                int nc = curr.second +d.second;
+                // if the adjacent cell is within bounds and has not been visited (i.e., has value -1),
+                // we update its distance and push it to the queue
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && mat[nr][nc] == -1){
+                    
+                    q.push({nr, nc});
+
+                    // The distance is updated by adding 1 to the distance of the current cell
+                    // This ensures that we are always finding the shortest distance to the nearest 0
+                    // This is because we are processing cells in the order of their distance from the nearest 0
+                    // and we are only updating cells that have not been visited yet
+                    mat[nr][nc] = mat[curr.first][curr.second] + 1;
+
+                }
+                
+            }
+        }
+
+        return mat;
+    }
+
+};
